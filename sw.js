e4e12bbe5aa62e75f7518f4897b88ba47bd8cfa9
IAN SHIN <77316617+jeongseobshin@users.noreply.github.com>
2026-07-12 22:26:03 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tbm-mgmt-v14';
+const CACHE = 'tbm-mgmt-v15';
 const ASSETS = ['./','./index.html','./tbm_app.js','./app-config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
@@ -7,10 +7,19 @@ self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  // 서버 함수/API, 비-GET, 타 출처 요청은 서비스워커가 절대 가로채지 않음(항상 네트워크)
   if (e.request.method !== 'GET') return;
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-    const copy = res.clone();
-    caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
-    return res;
-  }).catch(() => caches.match('./index.html'))));
+  if (url.origin !== self.location.origin) return;
+  if (url.pathname.includes('/.netlify/')) return;
+  // 정적 자산만 캐시. 실패해도 HTML로 대체하지 않음(그래야 JSON 응답이 오염되지 않음)
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res && res.ok && res.type === 'basic') {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      }
+      return res;
+    }))
+  );
 });

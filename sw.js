5014d5a5f8275b136a372e5d875a32a8499e3dc8
IAN SHIN <77316617+jeongseobshin@users.noreply.github.com>
2026-07-12 22:41:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tbm-mgmt-v15';
+const CACHE = 'tbm-mgmt-v16';
 const ASSETS = ['./','./index.html','./tbm_app.js','./app-config.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
